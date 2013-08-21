23dc450f379c7cf4ea46e17908a0c29aab673060
Markus Krogemann <markus@krogemann.de>
2013-08-21 15:42:08 +0200

    more skeleton (empty steps)

---
+++ features/step_definitions/webfinger_steps.rb
@@ -0,0 +1,11 @@
+Given(/^an existing user account 'ladila'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I make a request to 'well\-known\-ladila'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should receive a valid webfinger document$/) do
+  pending # express the regexp above with the code you wish you had
+end
