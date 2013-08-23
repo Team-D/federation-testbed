9ded7ca32ad33b1e8183ad7a9ac2cba444b57655
carolina <carolina@sindominio.net>
2013-08-23 15:53:38 +0200

    --

--- features/step_definitions/webfinger_steps.rb
+++ features/step_definitions/webfinger_steps.rb
@@ -1,11 +1,28 @@
-Given(/^an existing user account with id "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^an existing user account diaspora_user$/) do 
+  @diaspora_user = 'carolina@diaspora-fr.org'
 end
 
-When(/^I make a webfinger\-request to an existing diaspora pod with url "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I make a webfinger\-request to an existing diaspora pod with url$/) do
+  RestClient.get 'https://diaspora-fr.org//webfinger?q=acct:'+ @diaspora_user
 end
 
 Then(/^I should receive a valid webfinger document$/) do
+  response = RestClient.get 'https://diaspora-fr.org//webfinger?q=acct:' + @diaspora_user
+  response.code == '200'
+end
+
+Given(/^an existing webfinger document$/) do
+  @response = 'https://diaspora-fr.org//webfinger?q=acct:' + @diaspora_user
+  @webfinger = Nokogiri::XML(open(@response))
+end
+Then(/^I make a hcard\-request$/) do
+  @webfinger.xpath('//xmlns:Link').each do |hcard|
+    
+  end
+
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should receive a valid hcard document$/) do
   pending # express the regexp above with the code you wish you had
 end
--- features/webfinger.feature
+++ features/webfinger.feature
@@ -2,9 +2,14 @@ Feature: Webfinger request
   As a user of diaspora-federation
   I should be able to send a webfinger request to a diaspora pod
   And receive back valid information about a user account
-
+  Background:
+   Given an existing user account diaspora_user	
 
   Scenario: Existing user account
-  Given an existing user account with id "ID"
-  When I make a webfinger-request to an existing diaspora pod with url "URL"
-  Then I should receive a valid webfinger document
+    Then I make a webfinger-request to an existing diaspora pod with url
+    Then I should receive a valid webfinger document
+
+  Scenario: Existing a webfinger document
+   Given an existing webfinger document
+   Then I make a hcard-request
+   Then I should receive a valid hcard document
