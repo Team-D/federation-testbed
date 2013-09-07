31b883b82cc289d8de79272cbb64d5de19ac85b3
Markus Krogemann <markus@krogemann.de>
2013-09-07 16:42:22 +0200

    comment on step name

--- features/step_definitions/host_meta.rb
+++ features/step_definitions/host_meta.rb
@@ -2,6 +2,7 @@ When(/^I send a host meta request to an existing diaspora pod$/) do
   @response = RestClient.get @server + "/.well-known/host-meta"
 end
 
+#MK: What happens in this step does not reflect the name of the step
 Then(/^it should contain a link to the webfinger document$/) do
   @webfinger_url = @response.to_s.rpartition(/(https?):((\/\/)|(\\\\))+[\w\d:\#\@\%\/;$()~_?\+-=\\\.&]*/)[-2] + "mokus@joindiaspora.com"
   RestClient.get @webfinger_url
