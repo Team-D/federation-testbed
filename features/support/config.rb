571f475551d512c1426ea822d0fcce7690884889
Markus Krogemann <markus@krogemann.de>
2013-09-07 16:46:56 +0200

    align usage of single/double quotes

--- features/support/config.rb
+++ features/support/config.rb
@@ -4,7 +4,6 @@ def read_config
   config = YAML::load(File.open('features/support/config.yml'))
   @diaspora_user = config['diaspora_user']
   @server = config['server']
-  @guid = config["guid"]
-  @private_key = config["private_key"]
+  @guid = config['guid']
+  @private_key = config['private_key']
 end
-
